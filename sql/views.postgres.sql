9ea2556f33b31639c26b19b686f3646bb55f24da
Wells Oliver <wellsoliver@gmail.com>
2013-12-15 19:15:02 -0800

    Added views

--- sql/views.postgres.sql
+++ sql/views.postgres.sql
@@ -1,12 +1,23 @@
-drop view if exists vw_games;
+drop view if exists vw_games cascade;
 
 create view vw_games as
 select
-	*,	
 	case
 		when length(game_dt::text) = 3 then ('000'||game_dt::text)::date
 		when length(game_dt::text) = 4 then ('00'||game_dt::text)::date
 		when length(game_dt::text) = 5 then ('0'||game_dt::text)::date
 		else game_dt::text::date
-	end as game_date
+	end as game_date,
+	*
 from games;
+
+create view vw_events as
+select
+	extract(year from vw_games.game_date) as year,
+	vw_games.game_date,
+	events.*,
+	lkup.shortname_tx,
+	lkup.longname_tx
+from events
+inner join lkup_cd_event lkup on events.event_cd = lkup.value_cd
+inner join vw_games using (game_id);
