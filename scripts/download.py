85d39989de4baad8ea3e80f41ac550dc93891e86
nrbergeron <nrbergeron@gmail.com>
2015-03-05 10:45:28 -0500

    Added comments

--- scripts/download.py
+++ scripts/download.py
@@ -8,6 +8,9 @@ import sys
 
 from classes.fetcher import Fetcher
 
+# initialize variables / set defaults
+YEAR = False
+
 # load configs
 config = ConfigParser.ConfigParser()
 config.readfp(open('config.ini'))
@@ -17,30 +20,38 @@ config.readfp(open('config.ini'))
 options = {}
 options['verbose'] = config.get('debug', 'verbose')
 
+# load and evaluate download directory
 path = config.get('download', 'directory')
 absolute_path = os.path.abspath(path)
+
+# test for existence of download directory
+# create if does not exist
 try:
     os.chdir(absolute_path)
 except OSError:
     print "Directory %s does not exist, creating..." % absolute_path
     os.makedirs(absolute_path)
 
-YEAR = False
 
+# parse options list. Look for -y <year> or --year <year> options
+# exit on unrecognized option or option without argument
 try:
     opts, args = getopt.getopt(sys.argv[1:], "y:", ["year="])
 except getopt.GetoptError as e:
-    print 'invalid arguments'
+    print 'Invalid arguments. Exiting.'
     raise SystemExit
 
+# set year if passed in
 for o, a in opts:
     if o in ('-y', '--year'): YEAR = a
 
+# log next action
 if YEAR:
     print "fetching retrosheet files for year %s..." % YEAR
 else:
     print "fetching retrosheet files..."
 
+# parse retrosheet page for file and add urls to the queue
 queue = Queue.Queue()
 retrosheet_url = config.get('retrosheet', 'url')
 pattern = r'(\d{4}?)eve\.zip'
@@ -49,6 +60,7 @@ for match in re.finditer(pattern, urllib.urlopen(retrosheet_url).read(), re.S):
     url = 'http://www.retrosheet.org/events/%seve.zip' % match.group(1)
     queue.put(url)
 
+# set threads
 threads = []
 num_threads = config.getint('download', 'num_threads')
 
