6944f96660db711bb2c1987432a6f44382a6ba85
shinyorke <spirits.is.my.rader@gmail.com>
2015-11-08 16:15:02 +0900

    create migration script for retrosheet_mysql.py(mysql only)

--- scripts/retrosheet_mysql.py
+++ scripts/retrosheet_mysql.py
@@ -19,43 +19,66 @@ class RetrosheetMySql(object):
 
     DATABASE_ENGINE = 'mysql+pymysql'
     ENGINE = '{engine}://{user}:{password}@{host}/{database}'
-    MODULES = (
+    TABLES = (
         {
-            'table': 'teams',
+            'name': 'teams',
+            'header': False,
+            'year': False,
             'mask': '{path}/TEAM{year}*',
-            'select': 'SELECT * FROM teams WHERE team_id = {team_id}',
-            'insert': 'INSERT INTO teams VALUES ({values})',
+            'select': "SELECT * FROM teams WHERE team_id = '{key_0}'",
+            'where_index': [0],
+            'insert': 'INSERT INTO teams VALUES {values}',
         },
         {
-            'table': 'rosters',
+            'name': 'rosters',
+            'header': False,
+            'year': True,
             'mask': '{path}/*{year}*.ROS',
-            'select': 'SELECT * FROM rosters WHERE year = {year} AND player_id = {player_id} AND team_tx = {team_tx}',
-            'insert': 'INSERT INTO rosters VALUES ({values})',
+            'select': "SELECT * FROM rosters WHERE year = {year} AND player_id = '{key_0}' AND team_tx = '{key_1}'",
+            'where_index': [0, 5],
+            'insert': 'INSERT INTO rosters VALUES {values}',
         },
         {
-            'table': 'events',
-            'mask': '{path}/csv/events-{year}*.csv',
-            'select': 'SELECT * FROM events WHERE game_id = {game_id} AND event_id = {event_id}',
-            'insert': 'INSERT INTO events VALUES ({values})',
-        },
-        {
-            'table': 'games',
+            'name': 'games',
+            'header': True,
+            'year': False,
             'mask': '{path}/csv/games-{year}*.csv',
-            'select': 'SELECT * FROM games WHERE game_id = {game_id}',
-            'insert': 'INSERT INTO games VALUES ({values})',
+            'select': "SELECT * FROM games WHERE game_id = '{key_0}'",
+            'where_index': [0],
+            'insert': 'INSERT INTO games({columns}) VALUES {values}',
+        },
+        {
+            'name': 'events',
+            'header': True,
+            'year': False,
+            'mask': '{path}/csv/events-{year}*.csv',
+            'select': "SELECT * FROM events WHERE game_id = '{key_0}' AND event_id = '{key_1}'",
+            'where_index': [0, 96],
+            'insert': 'INSERT INTO events({columns}) VALUES {values}',
         },
     )
 
     def __init__(self, configfile: str):
+        """
+        initialize
+        :param configfile: configuration file
+        """
         # configuration
         config = ConfigParser()
         config.read(configfile)
         self.path = os.path.abspath(config.get('download', 'directory'))
+        self.record_check = config.getboolean('retrosheet_mysql', 'record_check')
+        self.multiple_insert_rows = config.getint('retrosheet_mysql', 'multiple_insert_rows')
         # connection
         self.connection = self._generate_connection(config)
 
     @classmethod
     def _generate_connection(cls, config: ConfigParser):
+        """
+        generate database connection
+        :param config: ConfigParser object
+        :return:
+        """
 
         try:
             database_engine = cls.DATABASE_ENGINE
@@ -80,22 +103,65 @@ class RetrosheetMySql(object):
 
         return db.connect()
 
-    def insert(self, year, csv_file):
-        for row in csv.reader(open(csv_file)):
-            print(row)
+    @classmethod
+    def _exists_record(cls, year: int, table: dict, csv_row: list, connection):
+        where = {'key_{i}'.format(i=i): csv_row[v] for i, v in enumerate(table['where_index'])}
+        where['year'] = year
+        sql = table['select'].format(**where)
+        res = connection.execute(sql)
+        if res.rowcount > 0:
+            return True
+        return False
 
-    def execute(self, year):
-        for module in self.MODULES:
-            for csv_file in glob(module['mask'].format(path=self.path, year=year)):
-                self.insert(year, csv_file)
+    def _multiple_insert(self, query: str, columns: list, values: list):
+        params = {
+            'columns': columns,
+            'values': ', '.join(values),
+        }
+        sql = query.format(**params)
+        try:
+            self.connection.execute(sql)
+        except Exception as e:
+            print(e)
+            print(sql)
+            raise SystemExit
 
-        self.connection.close()
+
+    def _create_record(self, year: int, csv_file: str, table: dict):
+        reader = csv.reader(open(csv_file))
+        headers = []
+        values = []
+        if table['header']:
+            headers = next(reader)
+        columns = ', '.join(headers)
+        for row in reader:
+            # Record Exists Check
+            if self.record_check and RetrosheetMySql._exists_record(year, table, row, self.connection):
+                continue
+            # append record values
+            if table['year']: row.insert(0, str(year))
+            values.append('("{rows}")'.format(rows='", "'.join(row)))
+            if len(values) == self.multiple_insert_rows:
+                # inserts
+                self._multiple_insert(table['insert'], columns, values)
+                values = []
+        if len(values) > 0:
+            # inserts
+            self._multiple_insert(table['insert'], columns, values)
+
+    def execute(self, year: int):
+        for table in self.TABLES:
+            for csv_file in glob(table['mask'].format(path=self.path, year=year)):
+                self._create_record(year, csv_file, table)
 
     @classmethod
     def run(cls, from_year: int, to_year: int, configfile: str):
         client = RetrosheetMySql(configfile)
+        # TODO ここはマルチプロセス化するかも
         for year in [year for year in range(from_year, to_year + 1)]:
             client.execute(year)
+        # TODO ここまでマルチプロセス化
+        client.connection.close()
 
 
 @click.command()
